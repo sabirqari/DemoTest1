eacecf93ce963c6e5a13789405e5595b10d3cafa
nasrinna <nasrin.ahmad@noorteck.com>
2025-02-12 11:42:35 -0500

    new class

---
+++ src/main/java/DemoTest1/Demo.java
@@ -0,0 +1,30 @@
+package DemoTest1;
+
+public class Demo {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Sabir");
+	}
+}
+
+
+/**
+
+  Step 1  ..> right click on project > teams > shear project
+  step 2..>  check use or create repository in parent folder of project
+  step 3  .. >  click project name in the table then create repository will be enabled
+  step 4 .. >  once create repository enabled then cleck
+  step 5 ..> click finosh 
+  this is my local repository
+
+
+now we need to create a project (repository) in GitHub the Repository name needs to be same as your project name
+
+in gitHub 
+1 > click Icon Select new repository
+2 > enter repository name : make sure it is same as your project name
+3 > click create repository
+
+*/
